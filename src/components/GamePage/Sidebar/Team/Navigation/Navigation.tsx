f4fedfecedcb50e1b584638f59674d06022c4112
Pavel Ivanov <grammka@gmail.com>
2021-02-19 22:48:44 +0300

    fix team navigation

--- src/components/GamePage/Sidebar/Team/Navigation/Navigation.tsx
+++ src/components/GamePage/Sidebar/Team/Navigation/Navigation.tsx
@@ -20,19 +20,11 @@ const Switch = () => {
 
 const Spymaster = () => {
   const game = useContext(GameContext)
-  const { player, players } = useContext(GameStateContext)
-
-  const teamPlayers = players.filter((p) => p.color === player?.color)
-  const isVisible = player && teamPlayers.filter((p) => p.spymaster).length === 0
 
   const handleClick = useCallback(() => {
     socket.emit('become spymaster')
     storage.removeItem(game.id)
   }, [ game ])
-
-  if (!isVisible) {
-    return null
-  }
 
   return (
     <div className={s.row}>
@@ -54,19 +46,31 @@ const Spectator = () => {
 }
 
 const Navigation = () => {
-  const { player, players, isFinished } = useContext(GameStateContext)
+  const { player, players } = useContext(GameStateContext)
 
-  const isPlayerJoinedATeam = Boolean(players.find((p) => p.id === player?.id))
+  const teamPlayers = players.filter((p) => p.color === player?.color)
 
-  if ((player?.spymaster && !isFinished) || !isPlayerJoinedATeam) {
-    return null
-  }
+  const isSwitchVisible = Boolean(player && !player.spymaster)
+  const isSpymasterVisible = Boolean(player && teamPlayers.filter((p) => p.spymaster).length === 0)
+  const isSpectatorVisible = Boolean(player)
 
   return (
     <div className={s.navigation}>
-      <Switch />
-      <Spymaster />
-      <Spectator />
+      {
+        isSwitchVisible && (
+          <Switch />
+        )
+      }
+      {
+        isSpymasterVisible && (
+          <Spymaster />
+        )
+      }
+      {
+        isSpectatorVisible && (
+          <Spectator />
+        )
+      }
     </div>
   )
 }
